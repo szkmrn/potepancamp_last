e9d04540f36216de8019f9d8c62c4785cd7a8ff0
keisuke suzuki <keisukesuzuki@keisuke.local>
2022-09-12 20:15:25 +0900

    不要なコメントの削除とletへ修正

--- spec/requests/potepans_spec.rb
+++ spec/requests/potepans_spec.rb
@@ -1,13 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe "Potepan:s", type: :request do
-  # describe "GET /potepan:s" do
-  #   it "works! (now write some real specs)" do
-  #     get potepan:s_path
-  #     expect(response).to have_http_status(200)
-  #   end
-  # end
-
   describe "GET potepan_path" do
     it "正常にレスポンスを返すこと" do
       get potepan_url
--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Products", type: :request do
   describe "GET /show" do
-    let!(:product) { create(:spree_product, :skip_validate) }
+    let(:product) { create(:spree_product, :skip_validate) }
 
     before do
       get potepan_product_url(product.id)
