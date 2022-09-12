bb8c07e28cbbb9392b55524033a1f8676d62485c
keisuke suzuki <keisukesuzuki@keisuke.local>
2022-09-12 20:51:51 +0900

    have_contentで画面に表示されていることがテストできるよう修正

--- spec/system/products_spec.rb
+++ spec/system/products_spec.rb
@@ -13,14 +13,16 @@ RSpec.describe "Products", type: :system do
       expect(current_path).to eq potepan_path
     end
 
-    it "商品名が取得されること" do
-      expect(product.name).to eq("sweater")
+    it "商品名が表示されること" do
+      expect(page).to have_content "sweater"
     end
-    it "商品価格が取得されること" do
-      expect(product.price).to eq(23.45)
+
+    it "商品価格が表示されること" do
+      expect(page).to have_content "23.45"
     end
-    it "商品説明が取得されること" do
-      expect(product.description).to eq("very hot!")
+
+    it "商品説明が表示されること" do
+      expect(page).to have_content "very hot!"
     end
   end
 end
