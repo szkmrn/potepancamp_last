669e5afccfd7c4f6d4539b6c522ec0ca9a265841
keisuke suzuki <keisukesuzuki@keisuke.local>
2022-09-23 14:21:08 +0900

    parent_idがnulの時商品を全て表示するよう変更

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -2,5 +2,6 @@ class Potepan::CategoriesController < ApplicationController
   def show
     @taxon = Spree::Taxon.find(params[:id])
     @taxons = Spree::Taxon.all
+    @products = Spree::Product.all
   end
 end
