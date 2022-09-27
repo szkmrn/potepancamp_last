4cbcae2267a8503202cd329bc625d916016a5f1e
keisuke suzuki <keisukesuzuki@keisuke.local>
2022-09-27 22:22:05 +0900

    リクエストスペックに表示テストの追加

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -2,8 +2,12 @@ require 'rails_helper'
 
 RSpec.describe "Categories", type: :request do
   describe "GET /show" do
-    let(:taxon) { create(:taxon) }
+    let!(:taxonomy) { create(:taxonomy) }
+    let(:taxon) { create(:taxon, taxonomy: taxonomy, parent: taxonomy.root) }
     let!(:product) { create(:product, taxons: [taxon]) }
+    let!(:polo) { create(:product, name: "POLO", taxons: [taxon]) }
+    let!(:taxon1) { create(:taxon, name: "RUBY", taxonomy: taxonomy, parent: taxonomy.root) }
+    let!(:product1) { create(:product, price: 23, taxons: [taxon1]) }
 
     before do
       get potepan_category_url(taxon.id)
@@ -13,12 +17,37 @@ RSpec.describe "Categories", type: :request do
       expect(response).to have_http_status(:success)
     end
 
-    it "商品名が表示されること" do
-      expect(response.body).to include product.name
-    end
+    describe "表示テスト" do
+      context "表示される場合のテスト" do
+        it "カテゴリー名(root)が表示されること" do
+          expect(response.body).to include taxonomy.name
+        end
 
-    it "商品価格が表示されること" do
-      expect(response.body).to include product.display_price.to_s
+        it "カテゴリー名が表示されること" do
+          expect(response.body).to include taxon.name
+          expect(response.body).to include taxon1.name
+        end
+
+        it "商品名が表示されること" do
+          expect(response.body).to include product.name
+          expect(response.body).to include polo.name
+        end
+
+        it "商品価格が表示されること" do
+          expect(response.body).to include product.display_price.to_s
+          expect(response.body).to include polo.display_price.to_s
+        end
+      end
+
+      context "表示されない場合のテスト" do
+        it "カテゴリー外の商品名が表示されないこと" do
+          expect(response.body).not_to include product1.name
+        end
+
+        it "カテゴリー外の商品価格が表示されないこと" do
+          expect(response.body).not_to include product1.display_price.to_s
+        end
+      end
     end
   end
 end
