e07d1992572cf744cce1200b3b1458886126aa69
keisuke suzuki <keisukesuzuki@keisuke.local>
2022-10-02 23:12:54 +0900

    N+1問題解消のための変更

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -1,7 +1,8 @@
 class Potepan::CategoriesController < ApplicationController
   def show
+    @taxonomies = Spree::Taxonomy.includes(taxons: :products)
+    @taxons = Spree::Taxon.leaves.includes(:products)
     @taxon = Spree::Taxon.find(params[:id])
-    @products = @taxon.all_products
-    @taxonomies = Spree::Taxon.roots
+    @products = @taxon.all_products.includes(master: [:images, :prices])
   end
 end
