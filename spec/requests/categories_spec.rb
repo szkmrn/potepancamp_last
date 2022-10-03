b9a8e4ff13db1b37be7c0de62f1c9145026fa101
keisuke suzuki <keisukesuzuki@keisuke.local>
2022-10-03 11:11:57 +0900

    変数名の修正と最小限のテストになるように不要なテストの削除

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -3,14 +3,13 @@ require 'rails_helper'
 RSpec.describe "Categories", type: :request do
   describe "GET /show" do
     let(:taxonomy) { create(:taxonomy) }
-    let(:taxon) { create(:taxon, taxonomy: taxonomy, parent: taxonomy.root) }
-    let!(:product) { create(:product, taxons: [taxon]) }
-    let!(:polo) { create(:product, name: "POLO", taxons: [taxon]) }
-    let!(:taxon1) { create(:taxon, name: "RUBY", taxonomy: taxonomy, parent: taxonomy.root) }
-    let!(:product1) { create(:product, price: 23, taxons: [taxon1]) }
+    let(:taxonomy_taxon) { create(:taxon, taxonomy: taxonomy, parent: taxonomy.root) }
+    let!(:taxon_product) { create(:product, taxons: [taxonomy_taxon]) }
+    let!(:taxonomy_taxon1) { create(:taxon, name: "RUBY", taxonomy: taxonomy, parent: taxonomy.root) }
+    let!(:taxon1_product) { create(:product, price: 23, taxons: [taxonomy_taxon1]) }
 
     before do
-      get potepan_category_path(taxon.id)
+      get potepan_category_path(taxonomy_taxon.id)
     end
 
     it "returns http success" do
@@ -22,29 +21,27 @@ RSpec.describe "Categories", type: :request do
         it "カテゴリー名が表示されること" do
           within(".mainContent") do
             expect(response.body).to include taxonomy.name
-            expect(response.body).to include taxon.name
-            expect(response.body).to include taxon1.name
+            expect(response.body).to include taxonomy_taxon.name
+            expect(response.body).to include taxonomy_taxon1.name
           end
         end
 
         it "商品名が表示されること" do
-          expect(response.body).to include product.name
-          expect(response.body).to include polo.name
+          expect(response.body).to include taxon_product.name
         end
 
         it "商品価格が表示されること" do
-          expect(response.body).to include product.display_price.to_s
-          expect(response.body).to include polo.display_price.to_s
+          expect(response.body).to include taxon_product.display_price.to_s
         end
       end
 
       context "表示されない場合のテスト" do
         it "カテゴリー外の商品名が表示されないこと" do
-          expect(response.body).not_to include product1.name
+          expect(response.body).not_to include taxon1_product.name
         end
 
         it "カテゴリー外の商品価格が表示されないこと" do
-          expect(response.body).not_to include product1.display_price.to_s
+          expect(response.body).not_to include taxon1_product.display_price.to_s
         end
       end
     end
--- spec/system/categories_spec.rb
+++ spec/system/categories_spec.rb
@@ -3,44 +3,43 @@ require 'rails_helper'
 RSpec.describe "Categories", type: :system do
   describe "GET /show" do
     let(:taxonomy) { create(:taxonomy) }
-    let(:taxon) { create(:taxon, taxonomy: taxonomy, parent: taxonomy.root) }
-    let!(:product) { create(:product, taxons: [taxon]) }
-    let!(:polo) { create(:product, name: "POLO", taxons: [taxon]) }
-    let!(:taxon1) { create(:taxon, name: "RUBY", taxonomy: taxonomy, parent: taxonomy.root) }
-    let!(:product1) { create(:product, price: 23, taxons: [taxon1]) }
+    let(:taxonomy_taxon) { create(:taxon, taxonomy: taxonomy, parent_id: taxonomy.root) }
+    let!(:taxon_product) { create(:product, taxons: [taxonomy_taxon]) }
+    let!(:taxonomy_taxon1) { create(:taxon, name: "RUBY", taxonomy: taxonomy, parent: taxonomy.root) }
+    let!(:taxon1_product) { create(:product, price: 23, taxons: [taxonomy_taxon1]) }
 
     before do
-      visit potepan_category_path(taxon.id)
+      visit potepan_category_path(taxonomy_taxon.id)
     end
 
     describe "リンクテスト" do
       it "カテゴリー名をクリックで該当の商品一覧に遷移すること" do
-        click_link taxon.name
-        expect(current_path).to eq potepan_category_path(taxon.id)
+        click_link taxonomy_taxon.name
+        expect(current_path).to eq potepan_category_path(taxonomy_taxon.id)
       end
 
       it "一覧表示の商品をクリックで商品詳細ページへ遷移" do
-        click_link product.name
-        expect(current_path).to eq potepan_product_path(product.id)
+        click_link taxon_product.name
+        expect(current_path).to eq potepan_product_path(taxon_product.id)
       end
     end
 
     describe "表示テスト" do
       context "表示される場合のテスト" do
         it "タイトルが表示されること" do
-          expect(page).to have_title "#{taxon.name} - BIGBAG Store"
+          expect(page).to have_title "#{taxonomy_taxon.name} - BIGBAG Store"
         end
 
         it "カテゴリー名が表示されること" do
           within(".mainContent") do
             expect(page).to have_content taxonomy.name
-            expect(page).to have_content taxon.name
-            expect(page).to have_content taxon1.name
+            expect(page).to have_content taxonomy_taxon.name
+            expect(page).to have_content taxonomy_taxon1.name
           end
         end
 
         it "カテゴリーに属する商品の数が表示されること" do
-          expect(page).to have_content "#{taxon.name} (#{taxon.products.count})"
+          expect(page).to have_content "#{taxonomy_taxon.name} (#{taxonomy_taxon.products.count})"
         end
 
         it "サイドバーに表示される商品数と一覧表示される商品の数が一致すること" do
@@ -48,23 +47,21 @@ RSpec.describe "Categories", type: :system do
         end
 
         it "商品名が表示されること" do
-          expect(page).to have_content product.name
-          expect(page).to have_content polo.name
+          expect(page).to have_content taxon_product.name
         end
 
         it "商品価格が表示されること" do
-          expect(page).to have_content product.display_price.to_s
-          expect(page).to have_content polo.display_price.to_s
+          expect(page).to have_content taxon_product.display_price.to_s
         end
       end
 
       context "表示されない場合のテスト" do
         it "カテゴリー外の商品名が表示されないこと" do
-          expect(page).not_to have_content product1.name
+          expect(page).not_to have_content taxon1_product.name
         end
 
         it "カテゴリー外の商品価格が表示されないこと" do
-          expect(page).not_to have_content product1.display_price.to_s
+          expect(page).not_to have_content taxon1_product.display_price.to_s
         end
       end
     end
