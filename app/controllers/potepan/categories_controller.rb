b31afb36c2c7d0842faffb7c963b5f02a5884a91
keisuke suzuki <keisukesuzuki@keisuke.AirPort>
2022-10-07 20:20:19 +0900

    N+1問題対応のための変更

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -1,8 +1,8 @@
 class Potepan::CategoriesController < ApplicationController
   def show
-    @taxonomies = Spree::Taxonomy.includes(taxons: :products)
+    @taxonomies = Spree::Taxonomy.eager_load(taxons: :products)
     @taxons = Spree::Taxon.leaves.includes(:products)
     @taxon = Spree::Taxon.find(params[:id])
-    @products = @taxon.all_products.includes(master: [:images, :prices])
+    @products = @taxon.all_products.eager_load(master: [:images, :prices])
   end
 end
