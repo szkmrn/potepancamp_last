629ea2aa084c6837a457bfb4760dcd0f8670e798
keisuke suzuki <keisukesuzuki@keisuke.AirPort>
2022-10-07 22:51:45 +0900

    変数名の修正

--- spec/system/categories_spec.rb
+++ spec/system/categories_spec.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 RSpec.describe "Categories", type: :system do
   describe "GET /show" do
     let(:taxonomy) { create(:taxonomy) }
-    let(:taxonomy_taxon) { create(:taxon, taxonomy: taxonomy, parent_id: taxonomy.root) }
-    let!(:taxon_product) { create(:product, taxons: [taxonomy_taxon]) }
-    let!(:taxonomy_taxon1) { create(:taxon, name: "RUBY", taxonomy: taxonomy, parent: taxonomy.root) }
-    let!(:taxon1_product) { create(:product, price: 23, taxons: [taxonomy_taxon1]) }
+    let(:taxon) { create(:taxon, taxonomy: taxonomy, parent_id: taxonomy.root) }
+    let(:taxon_product) { create(:product, taxons: [taxon]) }
+    let!(:taxon1) { create(:taxon, name: "RUBY", taxonomy: taxonomy, parent: taxonomy.root) }
+    let!(:taxon1_product) { create(:product, price: 23, taxons: [taxon1]) }
     let(:image) { create(:image) }
 
     before do
       taxon_product.images << image
-      visit potepan_category_path(taxonomy_taxon.id)
+      visit potepan_category_path(taxon.id)
       # 画像URL取得が上手くいかない問題への対応
       # https://mng-camp.potepan.com/curriculums/document-for-final-task-2#notes-of-image-test
       ActiveStorage::Current.host = page.current_host
@@ -19,8 +19,8 @@ RSpec.describe "Categories", type: :system do
 
     describe "リンクテスト" do
       it "カテゴリー名をクリックで該当の商品一覧に遷移すること" do
-        click_link taxonomy_taxon.name
-        expect(current_path).to eq potepan_category_path(taxonomy_taxon.id)
+        click_link taxon.name
+        expect(current_path).to eq potepan_category_path(taxon.id)
       end
 
       it "一覧表示の商品をクリックで商品詳細ページへ遷移" do
@@ -32,19 +32,19 @@ RSpec.describe "Categories", type: :system do
     describe "表示テスト" do
       context "表示される場合のテスト" do
         it "タイトルが表示されること" do
-          expect(page).to have_title "#{taxonomy_taxon.name} - BIGBAG Store"
+          expect(page).to have_title "#{taxon.name} - BIGBAG Store"
         end
 
         it "カテゴリー名が表示されること" do
           within(".mainContent") do
             expect(page).to have_content taxonomy.name
-            expect(page).to have_content taxonomy_taxon.name
-            expect(page).to have_content taxonomy_taxon1.name
+            expect(page).to have_content taxon.name
+            expect(page).to have_content taxon1.name
           end
         end
 
         it "カテゴリーに属する商品の数が表示されること" do
-          expect(page).to have_content "#{taxonomy_taxon.name} (#{taxonomy_taxon.products.count})"
+          expect(page).to have_content "#{taxon.name} (#{taxon.products.count})"
         end
 
         it "サイドバーに表示される商品数と一覧表示される商品の数が一致すること" do
