9f1f5838f0c146168aacd645d22b4db621615665
keisuke suzuki <keisukesuzuki@keisuke.AirPort>
2022-10-07 23:47:37 +0900

    不要なインスタンス変数の削除

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -1,7 +1,6 @@
 class Potepan::CategoriesController < ApplicationController
   def show
     @taxonomies = Spree::Taxonomy.all
-    @taxons = Spree::Taxon.leaves.includes(:products)
     @taxon = Spree::Taxon.find(params[:id])
     @products = @taxon.all_products.eager_load(master: [:images, :prices])
   end
