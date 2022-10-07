d5e4936a9ea7cdad5759ea072d7adfa9b2786076
keisuke suzuki <keisukesuzuki@keisuke.AirPort>
2022-10-07 22:06:32 +0900

    N+1を許容し、処理が簡素な実装にするため、@taxonomiesのeager_loadを削除

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -1,6 +1,6 @@
 class Potepan::CategoriesController < ApplicationController
   def show
-    @taxonomies = Spree::Taxonomy.eager_load(taxons: :products)
+    @taxonomies = Spree::Taxonomy.all
     @taxons = Spree::Taxon.leaves.includes(:products)
     @taxon = Spree::Taxon.find(params[:id])
     @products = @taxon.all_products.eager_load(master: [:images, :prices])
