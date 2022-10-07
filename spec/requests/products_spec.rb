1900e1ddf6072f7d045cca4798fc9ea48ef893f8
keisuke suzuki <keisukesuzuki@keisuke.AirPort>
2022-10-07 23:41:14 +0900

    requests/products_specに商品画像テストの追加

--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -4,9 +4,14 @@ RSpec.describe "Products", type: :request do
   describe "GET /show" do
     let(:product) { create(:product) }
     let!(:taxon) { create(:taxon, products: [product]) }
+    let(:image) { create(:image)}
 
     before do
+      product.images << image
       get potepan_product_path(product.id)
+      # 画像URL取得が上手くいかない問題への対応
+      # https://mng-camp.potepan.com/curriculums/document-for-final-task-2#notes-of-image-test
+      ActiveStorage::Current.host = request.base_url
     end
 
     it "returns http success" do
@@ -23,6 +28,13 @@ RSpec.describe "Products", type: :request do
 
     it "商品詳細が表示されていること" do
       expect(response.body).to include product.description
+    end
+
+    it "商品画像が表示されていること" do
+      product.images.reload.each do |image|
+        expect(response.body).to include image.attachment(:large)
+        expect(response.body).to include image.attachment(:small)
+      end    
     end
   end
 end
