96fcc1a60a0460edbcf47da92efae6ec6eb057dc
keisuke suzuki <keisukesuzuki@keisuke.local>
2022-10-08 20:53:13 +0900

    @taxonを@taxon_idの変数名に修正

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,6 +1,6 @@
 class Potepan::ProductsController < ApplicationController
   def show
     @product = Spree::Product.find(params[:id])
-    @taxon = @product.taxons.first.id
+    @taxon_id = @product.taxons.first.id
   end
 end
